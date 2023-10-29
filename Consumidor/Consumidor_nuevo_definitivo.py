d9f143063b999d99dff52b3ae49adbaa66b328d7
Felipe Ulloa <felipeulloa5a43@gmail.com>
2023-10-29 14:54:37 -0300

    fix: Ayudante

--- Consumidor/Consumidor_nuevo_definitivo.py
+++ Consumidor/Consumidor_nuevo_definitivo.py
@@ -1,29 +1,84 @@
-from kafka import KafkaConsumer
-from itertools import zip_longest
+from confluent_kafka import Consumer, TopicPartition, KafkaError
+from confluent_kafka.admin import AdminClient, NewTopic
+import json
+from collections import deque
 
-servidores_bootstrap = 'localhost:9092'   
-topics = ['Formulario-topic']
 
-# Creando grupos de consumidores para cada topic
-consumer_groups = [f'grupo_consumidores_{topic}' for topic in topics]
+def create_topic(topic_name):
+    admin_client_config = {
+        "bootstrap.servers": "localhost:9092"
+    }
+    admin_client = AdminClient(admin_client_config)
 
-# Creando consumidores para cada grupo
-consumers = [
-    KafkaConsumer(
-        *topics,
-        group_id=group,
-        bootstrap_servers=[servidores_bootstrap]
+    new_topic = NewTopic(
+        topic_name,
+        num_partitions=2,
+        replication_factor=1
     )
-    for group in consumer_groups
-]
 
-# Partition of topics
+    created_topic_futures = admin_client.create_topics([new_topic])
+
+    for topic, future in created_topic_futures.items():
+        try:
+            future.result()
+            print(f"Tema {topic} creado con éxito.")
+        except Exception as e:
+            print(f"Falló la creación del tema {topic}: {e}")
 
 
-# Crear un bucle infinito para consumir mensajes de manera alternada de cada consumidor
-while True:
-    for msgs in zip_longest(*consumers):
-        for i, msg in enumerate(msgs):
-            if msg is not None:
-                print(f"Grupo de consumidores: {consumer_groups[i]}\n")
-                print(f"Topic: {msg.topic}, Mensaje: {msg.value}")
+# Crear el tema
+new_topic_name = 'Formulario-topic'
+create_topic(new_topic_name)
+
+# Configuración del consumidor
+conf = {
+    'bootstrap.servers': 'localhost:9092',
+    'group.id': 'grupo_consumidores',
+    'auto.offset.reset': 'earliest',
+}
+
+consumer = Consumer(conf)
+consumer.subscribe([new_topic_name])
+
+# Colas para almacenar mensajes de cada partición
+queue_0 = deque()
+queue_1 = deque()
+
+# Consumir mensajes
+print("Esperando mensajes...")
+try:
+    while True:
+        msg = consumer.poll(timeout=1.0)  # timeout en milisegundos
+        if msg is None:
+            continue
+        if msg.error():
+            if msg.error().code() == KafkaError._PARTITION_EOF:
+                print(
+                    f"Fin de la partición alcanzado {msg.topic()}/{msg.partition()}")
+            else:
+                print(f"Error: {msg.error()}")
+        else:
+            if msg.partition() == 0:
+                queue_0.append(msg)
+            else:
+                queue_1.append(msg)
+
+        # Procesar mensajes de la partición 0 primero
+        while queue_0:
+            msg = queue_0.popleft()
+            key = msg.key().decode('utf-8') if msg.key() else None
+            value = json.loads(msg.value().decode(
+                'utf-8')) if msg.value() else None
+            print(f"Mensaje recibido: {key} : {value}")
+
+        # Procesar mensajes de la partición 1 si la partición 0 está vacía
+        if not queue_0:
+            while queue_1:
+                msg = queue_1.popleft()
+                key = msg.key().decode('utf-8') if msg.key() else None
+                value = json.loads(msg.value().decode(
+                    'utf-8')) if msg.value() else None
+                print(f"Mensaje recibido: {key} : {value}")
+
+finally:
+    consumer.close()
