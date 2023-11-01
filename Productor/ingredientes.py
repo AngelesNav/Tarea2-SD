f9ef91f334e21edb06e51d711aaef897583c49d3
Angeles Navarrete <106721742+AngelesNav@users.noreply.github.com>
2023-11-01 14:37:20 -0300

    Update ingredientes.py

--- Productor/ingredientes.py
+++ Productor/ingredientes.py
@@ -10,28 +10,34 @@ def notify_stock_shortage(data):
 
     producer.produce(topic, key="stock-shortage", value=json.dumps(data).encode('utf-8'))
     producer.flush()
-def csv_to_json(input_filename, output_filename):
+
+def csv_to_json(input_filename, selected_columns):
     try:
-        with open(input_filename, 'r') as csvfile:
-            # Lee el archivo CSV
+        with open(input_filename, 'r', encoding='utf-8') as csvfile:
             csv_reader = csv.DictReader(csvfile)
-            json_list = []
+            json_list = [row for row in csv_reader]
 
-            for row in csv_reader:
-                json_list.append(row)
+        # Seleccionar solo las columnas especificadas
+        selected_data_list = []
+        for row in json_list:
+            selected_data = {col: row[col] for col in selected_columns}
+            selected_data_list.append(selected_data)
 
-        with open(output_filename, 'w') as jsonfile:
-            json.dump(json_list, jsonfile, indent=2)
-
+        return selected_data_list
+    except FileNotFoundError:
+        print(f"El archivo {input_filename} no fue encontrado.")
+    except Exception as e:
+        print(f"Ocurrió un error: {e}")
        
     except FileNotFoundError:
         print(f"El archivo {input_filename} no fue encontrado.")
     except Exception as e:
         print(f"Ocurrió un error: {e}")
 
-csv_to_json('data2.csv', 'stock.json')
+selected_columns = ['id', 'Maestro_Motehuesillero', 'Stock'] 
+data_stock = csv_to_json('data.csv', selected_columns)
 
-with open('stock.json', 'r') as json_file:
-    stock_shortage_data = json.load(json_file)
+#with open('stock.json', 'r') as json_file:
+#    stock_shortage_data = json.load(json_file)
 
-notify_stock_shortage(stock_shortage_data)
+notify_stock_shortage(data_stock)
