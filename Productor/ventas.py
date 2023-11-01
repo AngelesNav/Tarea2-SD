a4cf02a3fd866de8e8a3791a79f835fd3621f0a4
Angeles Navarrete <106721742+AngelesNav@users.noreply.github.com>
2023-11-01 14:45:59 -0300

    Update ventas.py

--- Productor/ventas.py
+++ Productor/ventas.py
@@ -10,35 +10,25 @@ def record_sale(data):
     producer.produce(topic, key="sale", value=json.dumps(data).encode('utf-8'))
     producer.flush()
 
-def csv_to_json(input_filename, output_filename):
+def csv_to_json(input_filename, selected_columns):
     try:
-        with open(input_filename, 'r') as csvfile:
-            # Lee el archivo CSV
+        with open(input_filename, 'r', encoding='utf-8') as csvfile:
             csv_reader = csv.DictReader(csvfile)
+            json_list = [row for row in csv_reader]
 
-            # Inicializa una lista para almacenar los objetos JSON
-            json_list = []
+        # Seleccionar solo las columnas especificadas
+        selected_data_list = []
+        for row in json_list:
+            selected_data = {col: row[col] for col in selected_columns}
+            selected_data_list.append(selected_data)
 
-            # Itera sobre las filas del CSV
-            for row in csv_reader:
-                # Agrega cada fila como un objeto JSON a la lista
-                json_list.append(row)
-
-        # Escribe la lista de objetos JSON en un archivo JSON de salida
-        with open(output_filename, 'w') as jsonfile:
-            json.dump(json_list, jsonfile, indent=2)
-
-       
+        return selected_data_list
     except FileNotFoundError:
         print(f"El archivo {input_filename} no fue encontrado.")
     except Exception as e:
         print(f"Ocurrió un error: {e}")
 
-# Reemplaza 'entrada.csv' y 'salida.json' con los nombres de tus archivos reales
-csv_to_json('data.csv', 'venta.json')
+selected_columns = ['id', 'Maestro_Motehuesillero', 'ganancia'] 
+data_venta = csv_to_json('data.csv', selected_columns)
 
-with open('venta.json', 'r') as json_file:
-    sale_data = json.load(json_file)
-
-
-record_sale(sale_data)
+record_sale(data_venta)
