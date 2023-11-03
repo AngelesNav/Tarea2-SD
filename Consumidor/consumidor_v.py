176721ee2ad58b875c537fa5828d97590988469b
Angeles Navarrete <106721742+AngelesNav@users.noreply.github.com>
2023-11-03 00:50:08 -0300

    Update consumidor_v.py

--- Consumidor/consumidor_v.py
+++ Consumidor/consumidor_v.py
@@ -22,14 +22,20 @@ while True:
     for msgs in zip_longest(*consumers):
         for i, msg in enumerate(msgs):
             if msg is not None:
-                print(f"Grupo de consumidores: {consumer_groups[i]}")
                 try:
                     messages = json.loads(msg.value.decode('utf-8'))
                     if isinstance(messages, list):
                         for message in messages:
                             print(f"Mensaje recibido: {message}")
+                            # Verifica si el stock es igual a 0 -->No se logro :(
+                            #if 'Stock' in message and message['Stock'] == 0:
+                                # Genera un mensaje por la terminal
+                            #    print(f"------------------------")
+                            #    maestro_id = message.get('id', 'ID no disponible')
+                            #    print(f"¡El stock es 0 para el maestro de motehuesillero con ID: {maestro_id}!")
+                            #    print(f"Se le entrego un nuevo stock")
+                            #    print(f"------------------------")
                     else:
                         print(f"Mensaje no es una lista válida: {messages}")
                 except json.JSONDecodeError as e:
                     print(f"Error al decodificar JSON: {e}")
-
